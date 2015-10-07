951f5cdda637adc1f40159a2ba646e71e5b6917f
Chae Cramb <chaecramb@gmail.com>
2015-10-07 16:39:42 +0100

    delete extra file

--- trams.rb
+++ trams.rb
@@ -1,22 +1,23 @@
-@tram_one = ["airport", "gyle centre", "edinburgh park", "murrayfield", "haymarket", "princess street", "york place" ]
-
-@tram_two = ["saltire square", "west pilton", "telford road", "craigleith"]
-
-@tram_three = ["gallery of modern art", "haymarket", "eicc", "bread street", "tollcross", "the meadows"]
+@tram_routes = { 
+  one: ["airport", "gyle centre", "edinburgh park", "murrayfield", "haymarket", "princess street", "york place"], 
+  two: ["saltire square", "west pilton", "telford road", "craigleith", "haymarket"], 
+  three: ["gallery of modern art", "haymarket", "eicc", "bread street", "tollcross", "the meadows"] 
+}
 
 def get_tram(direction)
   puts
   print "What tram are you getting #{direction}? "
-  tram = gets.chomp.downcase
-  (tram == "one") || (tram == "two") || (tram == "three") ? tram : tram_error(direction)
+  tram = gets.chomp.downcase.to_sym
+  (tram == :one) || (tram == :two) || (tram == :three) ? tram : tram_error(direction)
 end
 
 def get_stop(tram)
   puts
-  puts "Tram #{tram} stops: #{instance_variable_get("@tram_#{tram}").join(', ')}."
+  stop_list = @tram_routes[tram]
+  puts "Tram #{tram} stops: #{stop_list.join(', ')}."
   print "What stop on tram #{tram}? "
   stop = gets.chomp.downcase
-  instance_variable_get("@tram_#{tram}").include?(stop) ? stop : stop_error(tram)
+  stop_list.include?(stop) ? stop : stop_error(tram)
 end
 
 def get_route
@@ -28,7 +29,15 @@ end
 
 def stops_travelled
   get_route
-  [@tram_one.index(@stop_on), @tram_one.index(@stop_off)].max - [@tram_one.index(@stop_on), @tram_one.index(@stop_off)].min
+  if @tram_on == @tram_off
+    stop_numbers = [@tram_routes[@tram_on].index(@stop_on), @tram_routes[@tram_off].index(@stop_off)]
+    stop_numbers.max - stop_numbers.min
+  else
+    intersect = (@tram_routes[@tram_on] & @tram_routes[@tram_off]).pop.to_s
+    first_leg = [@tram_routes[@tram_on].index(@stop_on), @tram_routes[@tram_on].index(intersect)]
+    second_leg = [@tram_routes[@tram_off].index(intersect), @tram_routes[@tram_off].index(@stop_off)]
+    (first_leg.max - first_leg.min) + (second_leg.max - second_leg.min) 
+  end
 end
 
 def stop_error(tram)
